c84bc33dc20c61053bd3de1ef71f6d20682463e9
hide-a1 <hide210g@gmail.com>
2024-04-15 23:57:37 +0900

    feat: set env

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://turwypqfyedeganaekey.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1cnd5cHFmeWVkZWdhbmFla2V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI2NzgzNjYsImV4cCI6MjAyODI1NDM2Nn0.OJF4vh_7EeZ5mZGnW75JMgtyMxyQl2pyQLLjNC72hQM',
+};
